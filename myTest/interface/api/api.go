7dce0827ed2fe44f235632b42326b25eebff7aa9
Duome <duomesiki@gmail.com>
2023-01-30 23:10:47 +0800

    修改乘客\注册和登录接口

--- myTest/interface/api/api.go
+++ myTest/interface/api/api.go
@@ -19,10 +19,11 @@ func UserRegister(c *gin.Context) {
 	err := c.Bind(req)
 	if err != nil {
 		http.ErrJson(c, err)
+		return
 	}
 
-	checkUser := &entity.TUser{Phone: req.Phone}
-	err = store.DB.First(checkUser).Error
+	checkUser := new(entity.TUser)
+	err = store.DB.Where("phone = ?", req.Phone).First(checkUser).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
 		log.Error().Err(err).Str("phone", req.Phone).Msg("乘客/注册")
 		http.ErrJson(c, err)
@@ -40,7 +41,7 @@ func UserRegister(c *gin.Context) {
 		http.ErrJson(c, err)
 		return
 	}
-	if err = store.DB.Create(&user).Error; err != nil {
+	if err = store.DB.Create(user).Error; err != nil {
 		log.Error().Err(err).Str("phone", req.Phone).Msg("乘客/注册")
 		http.ErrJson(c, err)
 		return
@@ -60,13 +61,17 @@ func UserLogin(c *gin.Context) {
 	}
 
 	checkUser := new(entity.TUser)
-	checkUser.Phone = req.Phone
-	err = store.DB.First(checkUser).Error
-	if err != nil {
+	err = store.DB.Where("phone = ?", req.Phone).First(checkUser).Error
+	if err == gorm.ErrRecordNotFound {
+		err = errors.New("用户未注册，请注册后登录")
 		log.Error().Err(err).Str("phone", req.Phone).Msg("乘客/登录")
 		http.ErrJson(c, err)
 		return
-	} else if err == nil && checkUser.Password != req.Password {
+	} else if err != nil {
+		log.Error().Err(err).Str("phone", req.Phone).Msg("乘客/登录")
+		http.ErrJson(c, err)
+		return
+	} else if checkUser.Password != req.Password {
 		err = errors.New("密码错误，请重新登录")
 		log.Error().Err(err).Str("phone", req.Phone).Msg("乘客/登录")
 		http.ErrJson(c, err)
@@ -83,11 +88,13 @@ func UserLogin(c *gin.Context) {
 
 func DriverRegister(c *gin.Context) {
 	req := new(dto.DriverRegisterReq)
+	_ = req
 
 }
 
 func DriverLogin(c *gin.Context) {
 	req := new(dto.DriverLoginReq)
+	_ = req
 }
 
 func Userlogout(c *gin.Context) {
@@ -95,31 +102,40 @@ func Userlogout(c *gin.Context) {
 
 func UserUpdate(c *gin.Context) {
 	req := new(dto.UserUpdateReq)
+	_ = req
 }
 
 func UserDetail(c *gin.Context) {
 	rsp := new(dto.UserDetailRsp)
+	_ = rsp
 }
 
 func Driverlogout(c *gin.Context) {}
 
 func DriverUpdate(c *gin.Context) {
 	req := new(dto.DriverUpdateReq)
+	_ = req
 }
 
 func DriverDetail(c *gin.Context) {
 	req := new(dto.DriverReq)
 	rsp := new(dto.DriverDetailRsp)
+	_ = rsp
+	_ = req
 }
 
 func CarDetail(c *gin.Context) {
 	req := new(dto.CarReq)
 	rsp := new(dto.CarDetailRsp)
+	_ = req
+	_ = rsp
 }
 
 func TrackCreate(c *gin.Context) {
 	req := new(dto.TrackCreateReq)
 	rsp := new(dto.TrackCreateRsp)
+	_ = req
+	_ = rsp
 }
 
 func TrackCancel(c *gin.Context) {
@@ -127,37 +143,50 @@ func TrackCancel(c *gin.Context) {
 
 func TrackStatusSet(c *gin.Context) {
 	req := new(dto.TrackSetReq)
-	rsp := new(dto.TrackSetRsp)
+	// rsp := new(dto.TrackSetRsp)
+	_ = req
+	// _ = rsp
 }
 
 func TrackUpdate(c *gin.Context) {
 	req := new(dto.TrackUpdateReq)
+	_ = req
 }
 
 func TrackCurrent(c *gin.Context) {
 	rsp := new(dto.TrackDetailRsp)
+	_ = rsp
 }
 
 func TrackList(c *gin.Context) {
 	req := new(dto.TrackListReq)
 	rsp := new(dto.TrackListRsp)
+	_ = req
+	_ = rsp
 }
 
 func TrackDetail(c *gin.Context) {
 	req := new(dto.TrackReq)
 	rsp := new(dto.TrackDetailRsp)
+	_ = req
+	_ = rsp
 }
 
 func OrderDetail(c *gin.Context) {
 	req := new(dto.OrderReq)
 	rsp := new(dto.OrderDetailRsp)
+	_ = req
+	_ = rsp
 }
 
 func PaymentNotifyPay(c *gin.Context) {
 	req := new(dto.PaymentNotifyPayReq)
+	_ = req
 }
 
 func PaymentDetail(c *gin.Context) {
 	req := new(dto.PaymentReq)
 	rsp := new(dto.PaymentDetailRsp)
+	_ = req
+	_ = rsp
 }
